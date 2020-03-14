da8d334aa16caccfa391f4c67644a3611fbb9ea9
Piotr Krzeminski <git@krzeminski.it>
2020-03-14 17:36:20 +0100

    Experimenting with ubyte

--- src/jvmMain/kotlin/it/krzeminski/kotlin/midi/VariableLengthQuantityReading.kt
+++ src/jvmMain/kotlin/it/krzeminski/kotlin/midi/VariableLengthQuantityReading.kt
@@ -7,13 +7,17 @@ import kotlin.experimental.and
  * @return Within the pair, the first element is the requested quantity,
  *         and the second element is the number of bytes read.
  */
+@ExperimentalUnsignedTypes
 fun DataInputStream.readVariableLengthQuantity(): Pair<Int, Int> {
-    var readValue: Int = 0
-    val oldestBitMask = 0b10000000.toByte()
-    val byte = readByte()
-
-//    if ((byte and oldestBitMask) != 0.toByte()) {
-
-//    }
-    return Pair(byte.toInt(), 1)
+//    var readValue: Int = 0
+//    val oldestBitMask = 0b10000000.toByte()
+//    val byte = readByte()
+//
+////    if ((byte and oldestBitMask) != 0.toByte()) {
+//
+////    }
+//    return Pair(byte.toInt(), 1)
+//    val ubyte = ubyteArrayOf(0x00u)
+//    val byte = ubyte.toByteArray()
+    return Pair(1, 2)
 }
--- src/jvmTest/kotlin/it/krzeminski/kotlin/midi/VariableLengthQuantityReadingTest.kt
+++ src/jvmTest/kotlin/it/krzeminski/kotlin/midi/VariableLengthQuantityReadingTest.kt
@@ -4,17 +4,36 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
-import java.lang.Integer.max
+import java.io.DataInputStream
 
+@ExperimentalUnsignedTypes
 class VariableLengthQuantityReadingTest : StringSpec({
     "edge cases from MIDI specification" {
         // Source: http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html
         forAll(
-            row(1, 5, 4),
-            row(1, 0, 1),
-            row(0, 0, 1)
-        ) { a, b, max ->
-            max(a, b) shouldBe max
+            row(ubyteArrayOf(0x00u), 0x00)
+//            row(byteArrayOf(0x40),                      0x40),
+//            row(byteArrayOf(0x7F),                      0x7F),
+//            row(byteArrayOf(0x81, 0x00),               0x80),
+//            row(byteArrayOf(0xC0, 0x00),               0x2000),
+//            row(byteArrayOf(0xFF, 0x7F),               0x3FFF),
+//            row(byteArrayOf(0x81, 0x80, 0x00),        0x4000),
+//            row(byteArrayOf(0xC0, 0x80, 0x00),        0x100000),
+//            row(byteArrayOf(0xFF, 0xFF, 0x7F),        0x1FFFFF),
+//            row(byteArrayOf(0x81, 0x80, 0x80, 0x00), 0x200000),
+//            row(byteArrayOf(0xC0, 0x80, 0x80, 0x00), 0x8000000),
+//            row(byteArrayOf(0xFF, 0xFF, 0xFF, 0x7F), 0xFFFFFFF)
+        ) { inputBytes, expectedQuantity ->
+            // given
+//            val test = inputBytes.asByteArray()
+            val test = DataInputStream(byteArrayOf(0x00).inputStream()).readVariableLengthQuantity()
+//            val encodedNumberAsStream = DataInputStream(inputBytes.asByteArray().inputStream())
+
+            // when
+//            val (readQuantity, readBytes) = encodedNumberAsStream.readVariableLengthQuantity()
+//
+//            readQuantity shouldBe expectedQuantity
+//            readBytes shouldBe inputBytes.size
         }
     }
 })
@@ -25,18 +44,18 @@ class VariableLengthQuantityReadingTest : StringSpec({
 ////    @Test
 ////    fun validCase1() {
 ////        listOf(
-////            Pair(ubyteArrayOf(0x00u),                      0x00),
-////            Pair(ubyteArrayOf(0x40u),                      0x40),
-////            Pair(ubyteArrayOf(0x7Fu),                      0x7F)
-////            Pair(ubyteArrayOf(0x81u, 0x00u),               0x80),
-////            Pair(ubyteArrayOf(0xC0u, 0x00u),               0x2000),
-////            Pair(ubyteArrayOf(0xFFu, 0x7Fu),               0x3FFF),
-////            Pair(ubyteArrayOf(0x81u, 0x80u, 0x00u),        0x4000),
-////            Pair(ubyteArrayOf(0xC0u, 0x80u, 0x00u),        0x100000),
-////            Pair(ubyteArrayOf(0xFFu, 0xFFu, 0x7Fu),        0x1FFFFF),
-////            Pair(ubyteArrayOf(0x81u, 0x80u, 0x80u, 0x00u), 0x200000),
-////            Pair(ubyteArrayOf(0xC0u, 0x80u, 0x80u, 0x00u), 0x8000000),
-////            Pair(ubyteArrayOf(0xFFu, 0xFFu, 0xFFu, 0x7Fu), 0xFFFFFFF)
+////            row(byteArrayOf(0x00),                      0x00),
+////            row(byteArrayOf(0x40),                      0x40),
+////            row(byteArrayOf(0x7F),                      0x7F)
+////            row(byteArrayOf(0x81, 0x00),               0x80),
+////            row(byteArrayOf(0xC0, 0x00),               0x2000),
+////            row(byteArrayOf(0xFF, 0x7F),               0x3FFF),
+////            row(byteArrayOf(0x81, 0x80, 0x00),        0x4000),
+////            row(byteArrayOf(0xC0, 0x80, 0x00),        0x100000),
+////            row(byteArrayOf(0xFF, 0xFF, 0x7F),        0x1FFFFF),
+////            row(byteArrayOf(0x81, 0x80, 0x80, 0x00), 0x200000),
+////            row(byteArrayOf(0xC0, 0x80, 0x80, 0x00), 0x8000000),
+////            row(byteArrayOf(0xFF, 0xFF, 0xFF, 0x7F), 0xFFFFFFF)
 ////        ).forEach { (inputByteArray: UByteArray, expectedNumber: Int) ->
 ////            val inputStream = DataInputStream(inputByteArray.asByteArray().inputStream())
 ////            assertEquals(expectedNumber, inputStream.readVariableLengthQuantity().first)
@@ -47,7 +66,7 @@ class VariableLengthQuantityReadingTest : StringSpec({
 //    @Test
 //    fun testCaseFromSpecs_00() {
 //        // given
-//        val encodedNumberAsStream = DataInputStream(ubyteArrayOf(0x00u).asByteArray().inputStream())
+//        val encodedNumberAsStream = DataInputStream(byteArrayOf(0x00).asByteArray().inputStream())
 //        val actualQuantity = 0x00
 //
 //        // when
@@ -59,7 +78,7 @@ class VariableLengthQuantityReadingTest : StringSpec({
 //    @Test
 //    fun testCaseFromSpecs_40() {
 //        // given
-//        val encodedNumberAsStream = DataInputStream(ubyteArrayOf(0x40u).asByteArray().inputStream())
+//        val encodedNumberAsStream = DataInputStream(byteArrayOf(0x40).asByteArray().inputStream())
 //        val actualQuantity = 0x40
 //
 //        // when
@@ -71,7 +90,7 @@ class VariableLengthQuantityReadingTest : StringSpec({
 //    @Test
 //    fun testCaseFromSpecs_7F() {
 //        // given
-//        val encodedNumberAsStream = DataInputStream(ubyteArrayOf(0x7Fu).asByteArray().inputStream())
+//        val encodedNumberAsStream = DataInputStream(byteArrayOf(0x7F).asByteArray().inputStream())
 //        val actualQuantity = 0x7F
 //
 //        // when
